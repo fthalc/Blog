7fa952a1832298a87ec5a29ea28e4730de2085af
Fatih Alıcı <fatih.alici44@gmail.com>
2021-03-31 14:36:35 +0300

    Add project files.

---
+++ ProgrammersBlog.Shared/Entities/Abstract/EntityBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Entities.Abstract
+{
+    public abstract class EntityBase
+    {
+        public virtual int Id { get; set; }
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now;// override CreatedDate = new DateTime(2021/03/31)
+        public virtual DateTime ModifiedDate { get; set; }
+        public virtual bool IsDeleted { get; set; } = false;
+        public virtual bool IsActive { get; set; } = true;
+        public virtual String CreatedByName { get; set; }
+        public virtual String ModifiedByName { get; set; }
+
+    }
+}
