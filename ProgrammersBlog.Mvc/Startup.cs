503823e6ab31dab19cf9ec7436f110f82b616d68
Fatih Alıcı <fatih.alici44@gmail.com>
2021-04-30 17:33:19 +0300

    MVC

--- ProgrammersBlog.Mvc/Startup.cs
+++ ProgrammersBlog.Mvc/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProgrammersBlog.Services.Extensions;
 
 namespace ProgrammersBlog.Mvc
 {
@@ -19,7 +20,7 @@ namespace ProgrammersBlog.Mvc
         {
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddAutoMapper(typeof(Startup));
-            //services.LoadMyServices();
+            services.LoadMyServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
